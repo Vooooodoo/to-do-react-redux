147c24e741f2d352a615ca078209419e8b678e98
FusionTech <fusion@fusion.com>
2021-03-01 12:14:07 +0300

    refactor: remake reducer if constructions on switch case

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,55 +1,59 @@
-import * as actionTypes from '../constants/actionTypes'; //!
+import * as actionTypes from '../constants/actionTypes';
 import initialState from '../initialState';
 
 function reducer(state = initialState, action) {
-  if (action.type === actionTypes.SET_CREATE_INPUT_VALUE) {
-    return {
-      ...state,
-      createInputValue: action.payload,
-    }
+  switch (action.type) {
+    case actionTypes.SET_CREATE_INPUT_VALUE:
+      return {
+        ...state,
+        createInputValue: action.payload,
+      }
+
+    case actionTypes.SET_IS_CREATE_INPUT_VALUE_MAX_LENGTH:
+      return {
+        ...state,
+        isCreateInputMaxLength: action.payload,
+      }
+
+    case actionTypes.SET_EDIT_INPUT_VALUE:
+      return {
+        ...state,
+        editInputValue: action.payload,
+      }
+
+    case actionTypes.SET_IS_EDIT_INPUT_VALUE_MAX_LENGTH:
+      return {
+        ...state,
+        isEditInputMaxLength: action.payload,
+      }
+
+    case actionTypes.SET_TO_DO_ITEMS:
+      return {
+        ...state,
+        toDoItems: action.payload,
+      }
+
+    case actionTypes.SET_RADIO_VALUE:
+      return {
+        ...state,
+        radioValue: action.payload,
+      }
+
+    case actionTypes.SET_IS_ALL_COMPLETED:
+      return {
+        ...state,
+        isAllCompleted: action.payload,
+      }
   }
 
-  if (action.type === SET_IS_CREATE_INPUT_VALUE_MAX_LENGTH) {
-    return {
-      ...state,
-      isCreateInputMaxLength: action.payload
-    }
-  }
 
-  if (action.type === SET_EDIT_INPUT_VALUE) {
-    return {
-      ...state,
-      editInputValue: action.payload
-    }
-  }
 
-  if (action.type === SET_IS_EDIT_INPUT_VALUE_MAX_LENGTH) {
-    return {
-      ...state,
-      isEditInputMaxLength: action.payload
-    }
-  }
-
-  if (action.type === SET_TO_DO_ITEMS) {
-    return {
-      ...state,
-      ...action.payload
-    }
-  } //!
-
-  if (action.type === SET_RADIO_VALUE) {
-    return {
-      ...state,
-      radioValue: action.payload
-    }
-  }
-
-  if (action.type === SET_IS_ALL_COMPLETED) {
-    return {
-      ...state,
-      isAllCompleted: action.payload
-    }
-  }
+  // if (action.type === SET_TO_DO_ITEMS) {
+  //   return {
+  //     ...state,
+  //     ...action.payload
+  //   }
+  // } //!
 
   return state;
 }
